d19f1632cc7b323e29a0fea53e872d5415740a2f
Harsha G R <Harsha G R@DESKTOP-TOSMLO0>
2024-08-24 06:43:36 +0530

    done

---
+++ DWSFrameWork/src/test/java/com/automation/sampletestscripts/testcase_Test.java
@@ -0,0 +1,17 @@
+package com.automation.sampletestscripts;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class testcase_Test {
+	@Test
+	public void main()
+	{
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.instagram.com/harshu.gr/");
+		driver.close();
+		driver.quit();
+	}
+}
